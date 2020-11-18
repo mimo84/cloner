27786434b9f48f7010dc995bf090548fde49dedc
Maurizio Napoleoni <napy84@gmail.com>
2020-11-18 14:31:48 +1000

    for loop

--- bitbucket.sh
+++ bitbucket.sh
@@ -12,15 +12,12 @@ cloningFolder=bitbucket_repos
 rm -rf "${cloningFolder}"
 mkdir "${cloningFolder}"
 cd "${cloningFolder}"
-curl -u $user 'https://api.bitbucket.org/2.0/repositories?pagelen=100&role=owner' > owner.json
-curl -u $user 'https://api.bitbucket.org/2.0/repositories?pagelen=100&role=member' > member.json
-curl -u $user 'https://api.bitbucket.org/2.0/repositories?pagelen=100&role=contributor' > contributor.json
-curl -u $user 'https://api.bitbucket.org/2.0/repositories?pagelen=100&role=admin' > admin.json
-jq -r '.values[] | .links.clone[1].href' owner.json > personalrepos.txt
-jq -r '.values[] | .links.clone[1].href' member.json >> personalrepos.txt
-jq -r '.values[] | .links.clone[1].href' contributor.json >> personalrepos.txt
-jq -r '.values[] | .links.clone[1].href' admin.json >> personalrepos.txt
 
+for role in owner member contributor admin
+do
+  curl -u $user "https://api.bitbucket.org/2.0/repositories?pagelen=100&role=${role}" > "${role}.json"
+  jq -r '.values[] | .links.clone[1].href' "${role}.json" > personalrepos.txt
+done
 
 for repo in `cat personalrepos.txt`
 do
