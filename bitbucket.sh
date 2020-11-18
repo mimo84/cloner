d3554e3dba5017c35ec955ec2ab1685b4fa3e68a
Maurizio Napoleoni <napy84@gmail.com>
2020-11-18 15:55:13 +1000

    running in parallel

--- bitbucket.sh
+++ bitbucket.sh
@@ -12,23 +12,24 @@ cd ..
 cloningFolder=bitbucket_repos
 mkdir -p "${cloningFolder}"
 cd "${cloningFolder}"
-rm personalrepos.txt
-for role in owner member contributor admin
-do
-  curl -u $user "https://api.bitbucket.org/2.0/repositories?pagelen=100&role=${role}" > "${role}.json"
-  jq -r '.values[] | .links.clone[1].href' "${role}.json" >> personalrepos.txt
-  rm "${role}.json"
-done
+# rm personalrepos.txt
+# for role in owner member contributor admin
+# do
+#   curl -u $user "https://api.bitbucket.org/2.0/repositories?pagelen=100&role=${role}" > "${role}.json"
+#   jq -r '.values[] | .links.clone[1].href' "${role}.json" >> personalrepos.txt
+#   rm "${role}.json"
+# done
 
 # remove duplicates
 awk '!a[$0]++' personalrepos.txt > gitlist.txt
 
-for repo in `cat gitlist.txt`
-do
+current_directory=`pwd`
+
+run_the_clone() {
   # Gets the user/project/team/etc of the repo to put it in a separate folder
   project_folder=`echo $repo | cut -d':' -f2 | cut -d'/' -f1`
-  mkdir -p "${project_folder}"
-  cd "${project_folder}"
+  mkdir -p "${current_directory}/${project_folder}"
+  cd "${current_directory}/${project_folder}"
 
   # If the repo exists then simply perform a git pull to update the project.
   # Assumes that changes have been already committed / pushed to the remote
@@ -37,17 +38,25 @@ do
   if [ -d "${repo_folder}" ]
   then
     # echo "YES ${repo_folder} does exist"
+    (
       cd "${repo_folder}"
       echo -e '\033[1;32m' "Updating repo ${repo}" '\033[0m'
       git pull
-      cd ..
+    )
   else
     echo -e '\033[1;33m' "Cloning repo ${repo}" '\033[0m'
     git clone $repo
   fi
-  cd ..
+}
+
+(
+for repo in `cat gitlist.txt`
+do
+  run_the_clone "$repo" &
 done
-rm personalrepos.txt
-rm gitlist.txt
-cd ..
+)
+
+# rm personalrepos.txt
+# rm gitlist.txt
+# cd ..
 echo -e '\033[1;32m' "Done!" '\033[0m'
